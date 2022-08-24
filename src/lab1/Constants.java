c1592e68c43e793fc460092d5390eee49628b833
solid-ruslanrudenko <ruslan.rudenko@solid.software>
2022-08-24 20:56:09 +0200

    Initial commit

---
+++ src/lab1/Constants.java
@@ -0,0 +1,5 @@
+package lab1;
+
+public class Constants {
+    static double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2.0;
+}
---
+++ src/lab1/Lab1.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        new Lab1().run(args);
+    }
+
+    void run(String[] args) {
+
+    }
+}
